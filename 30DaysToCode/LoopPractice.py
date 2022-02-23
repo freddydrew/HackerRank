2f98e42dc67537084afd1a01ca6a3b816ed85b98
Freddy Drew <freddydrew@gmail.com>
2022-02-22 22:08:33 -0500

    Completed up to Day 6 in Notebook, changes to LoopPractice, Added MadLibs

--- 30DaysToCode/LoopPractice.py
+++ 30DaysToCode/LoopPractice.py
@@ -4,10 +4,12 @@ See the Jupyter Notebook for more notes related to this tutorial.
 """
 
 #practice with while loops
-from re import X
-
 #While loops, check condition then execute block
+from unicodedata import name
+
+
 def PracticeWhileLoop(x):
+    print('EXECUTING WHILE LOOP')
     while x < 5:
         print(f'The value of x is {x}')
         #x = x + 1
@@ -17,6 +19,7 @@ PracticeWhileLoop(0)
 #Do-While loops (dont exist in python so we change a while loop), 
 #execute the block then check condition 
 def PracticeDoWhileLoop(x):
+    print('EXECUTING DO-WHILE LOOP')
     while True:
         print(f'The value of x is {x}')
         #x = x + 1
@@ -25,3 +28,25 @@ def PracticeDoWhileLoop(x):
             break
 print()
 PracticeDoWhileLoop(0)
+
+def PracticeForLoop(x):
+    if x < 0:
+        inc = -1
+    else:
+        inc = 1
+    print('EXECUTING FOR LOOP')
+    for i in range(0,x,inc):
+        print(f'The value of x is {i}')
+
+print()
+PracticeForLoop(20)
+
+#nested for loops are loops within loops
+def PracticeNestedForLoop(n):
+    print('EXECUTING FOR LOOP WITH NESTED FOR LOOP')
+    for x in range (0,n+1):
+        for y in range(0,n+1): #the nested for loop
+            print(f'(x,y) = ({x},{y})')
+
+print()
+PracticeNestedForLoop(5)
---
+++ 30DaysToCode/MadLibs.py
@@ -0,0 +1,5 @@
+"""
+Included here: Days 6 of 30 Days to Code.
+See the Jupyter Notebook for more notes related to this tutorial.
+"""
+
